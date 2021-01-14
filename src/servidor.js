7937d5b3b0ef584876ab6a0fe9bbba367079a0cb
Rodrigo Lopes <rodrigolopesti1@gmail.com>
2021-01-13 22:34:48 -0300

    terminando projeto com node e express

--- src/servidor.js
+++ src/servidor.js
@@ -1,9 +1,37 @@
 const porta = 3003
 const express = require('express')
+const Banco = require('./banco')
+const bodyparser = require('body-parser')
 
 const app = express()
+
+app.use(bodyparser.urlencoded({extended: true}))
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: "notebook", preco: 1340.42, }) // vai ser convertido para json 
+    res.send(Banco.getProdutos()) // vai ser convertido para json 
+})
+app.get('/produtos/:id', (req, res, next) =>{
+    res.send(Banco.getProduto(req.params.id))
+})
+app.post('/produtos', (req, res, next) =>{
+    const produto = Banco.salvarProdutos({
+        nome: req.body.name,
+        preco: req.body.preco
+
+    })
+    req.send(produto) //JSON
+})
+app.put('/produtos/:id', (req, res, next) =>{
+    const produto = Banco.salvarProdutos({
+        id: req.params.id,
+        nome: req.body.name,
+        preco: req.body.preco
+
+    })
+    req.send(produto) //JSON
+})
+pp.delete('/produtos/:id', (req, res, next) =>{
+    const produto = Banco.excluirProdutos(req.params.id)
+    req.send(produto) //JSON
 })
 app.listen(porta, ()=>{
     console.log(`servidor executando na porta ${porta}.`)
